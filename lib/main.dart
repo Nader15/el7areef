f46c12fb845243167d3e1c167d33d30505918999
nader <nadersalah20201111@gmail.com>
2021-11-21 20:42:22 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:el7areef/Ui/Screens/SplahScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+    // home: CreateTeam(),
+  ));
+}
