4ee524968c02c6394407289f505230ab5da6e42c
mohamedelshafey <mohamed.elshafey7777@gmail.com>
2021-11-21 21:48:43 +0200

    any

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:el7areef/Ui/Screens/SplahScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: SplashScreen(),
